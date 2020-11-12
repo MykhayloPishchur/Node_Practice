52034f63f19bdc6a33be5eb5d3164ca4ca2d8ae5
Michael <mykhaylo88@icloud.com>
2020-11-12 15:08:51 +0200

    add

---
+++ auth/user.model.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const mongoosePaginate = require("mongoose-paginate-v2");
+
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  avatarURL: { type: String },
+  subscription: {
+    type: String,
+    enum: ["free", "pro", "premium"],
+    default: "free",
+    required: true,
+  },
+  token: { type: String },
+});
+
+userSchema.plugin(mongoosePaginate);
+userSchema.statics.findUserByIdAndUpdate = findUserByIdAndUpdate;
+userSchema.statics.findUserByEmail = findUserByEmail;
+userSchema.statics.findUserByToken = findUserByToken;
+userSchema.statics.updateToken = updateToken;
+
+async function findUserByIdAndUpdate(userId, updateParams) {
+  return this.findByIdAndUpdate(
+    userId,
+    {
+      $set: updateParams,
+    },
+    {
+      new: true,
+    }
+  );
+}
+
+async function findUserByEmail(email) {
+  return this.findOne({ email });
+}
+
+async function findUserByToken(token) {
+  return this.findOne({ token });
+}
+
+async function updateToken(id, newToken) {
+  return this.findByIdAndUpdate(id, {
+    token: newToken,
+  });
+}
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
